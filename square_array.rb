0a5e36d49cfd58160670081011f8d75eb6dab91d
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-08 18:14:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,5 @@
 def square_array(array)
-  numbers = [1, 2, 3]
-  square_array.each = numbers.collect {|number| number * number}
+  numbers = []
+  array.each = {|number| numbers << number ** 2}
+  numbers
 end
