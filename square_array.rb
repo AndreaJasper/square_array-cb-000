43eb2ff33dc9a909ae9adf622fd0280fd37599f1
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-08 19:52:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  squared_array = []
-  array.each = { |num|
-    squared_array << num ** 2
+  numbers = []
+  array.each { |num|
+    numbers << num ** 2
   }
-  squared_array
+  numbers
 end
